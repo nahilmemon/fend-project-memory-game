0ba5c8266add66696470f1c1a1c6882b37e515da
Nahil Memon <18367304+nahilmemon@users.noreply.github.com>
2018-04-04 21:07:25 +0400

    feat: Update HTML with new card deck on page load

--- js/app.js
+++ js/app.js
@@ -1,16 +1,64 @@
-/*
- * Create a list that holds all of your cards
- */
+// ------ Global Variables ------ //
+const arrayOfPossibleSymbols = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'];
+const numOfCards = 16;
+// ------ Selectors ------ //
+cardDeck = document.querySelector('.deck');
 
+// ------ Functions ------ //
+// Create a new card deck array from the given array of possible symbols
+function createNewCardDeckArray(arrayOfAllSymbols, sizeOfCardDeck) {
+  // Determine the number of card symbols needed
+  // One card deck should have half the number of symbols as there are cards
+  // Round this number up in case an odd number is given
+  let numOfCardSymbols = Math.ceil(sizeOfCardDeck/2);
+  // Make sure that numOfCardSymbols is a reasonable number
+  if (numOfCardSymbols < 2) {
+    // Should have at least two pairs
+    numOfCardSymbols = 2;
+  } else if (numOfCardSymbols > arrayOfAllSymbols.length) {
+    // should have no more pairs than what is available to choose from
+    numOfCardSymbols = arrayOfAllSymbols.length;
+  }
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+  // Shuffle the possible symbols array
+  shuffle(arrayOfAllSymbols);
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+  // Create a new card deck array from the array of possible symbols:
+  // Get the number of symbols needed
+  let newCardDeckArray = arrayOfAllSymbols.slice(0, numOfCardSymbols);
+  // Duplicate these symbols
+  newCardDeckArray = [...newCardDeckArray, ...newCardDeckArray];
+  // Shuffle the deck
+  shuffle(newCardDeckArray);
+
+  return newCardDeckArray;
+}
+
+// Delete the old card deck HTML,
+// create a new card deck array,
+// and add this new card deck to the deck ul list
+function updateHTMLWithNewCardDeck(sizeOfCardDeck) {
+  // Delete the old card deck HTML
+  cardDeck.innerHTML = '';
+
+  // Create new card deck array
+  const newCardDeckArray = createNewCardDeckArray(arrayOfPossibleSymbols, numOfCards);
+
+  // Create a document fragment to store the new HTML while it's being generated
+  const docFragment = document.createDocumentFragment();
+  // Create a bunch of card elements and add it to the docFragment
+  for (let i = 0; i < sizeOfCardDeck; i++) {
+    const newCardElement = document.createElement('li');
+    newCardElement.classList.add('card');
+    newCardElement.innerHTML = `<i class="fa ${newCardDeckArray[i]}"></i>`;
+    docFragment.appendChild(newCardElement);
+  }
+  // Add the new card deck HTML from the docFragment
+  cardDeck.appendChild(docFragment); // reflow and repaint here -- once!
+}
+
+// Shuffle the items in a given array
+// Note: this function was taken from http://stackoverflow.com/a/2450976
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -25,14 +73,8 @@ function shuffle(array) {
   return array;
 }
 
+// ------ Initialization ------ //
+// Add a new card deck to the HTML
+updateHTMLWithNewCardDeck(numOfCards);
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// ------ Events ------ //
